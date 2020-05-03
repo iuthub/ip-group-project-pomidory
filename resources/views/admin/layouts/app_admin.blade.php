1aaaba64577932056211e1e14c54bc3cc174fcc4
Abdukodir <carter.devper@mail.ru>
2020-05-03 18:49:13 +0500

    add category

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -27,6 +27,13 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+
+
+        <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
     {{-- <link href="{{ asset('css/main.css') }}" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -53,7 +60,7 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                                 aria-expanded="false">Blog</a>
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Category</a></li>
+                            <li><a href="{{route('admin.category.index')}}">Category</a></li>
                                 <li><a href="#">Documents</a></li>
                             </ul>
                         </li>
